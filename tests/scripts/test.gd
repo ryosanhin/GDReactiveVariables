6ca9b63583c9e8d1bc3d5a60bd4f2914f98a3e95
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-06-20 21:33:02 +0900

    test: テスト用のスクリプトとシーンを追加

---
+++ tests/scripts/test.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var _health := ReactiveVariable.new(100.0)
+
+func _ready() -> void:
+	_health.filter(func(v): return v > 50.0) \
+		.skip(1) \
+		.map(func(v): return "HP" + str(v)) \
+		.subscribe(func(msg): print(msg)) \
+		.add_to(self)
+	
+	_health.filter(func(v): return v < 50.0) \
+		.map(func(v): return "Warning! HP" + str(v)) \
+		.subscribe(func(msg): print(msg)) \
+		.add_to(self)
+	
+	print("-----start test-----")
+	
+	_health.value = 90.0
+	
+	_health.value = 70.0
+	
+	_health.value = 40.0
+	
+	_health.value = 10.0
+	
+	print("-----finish test-----")
+	
+	await get_tree().create_timer(5).timeout
+	
+	queue_free()
