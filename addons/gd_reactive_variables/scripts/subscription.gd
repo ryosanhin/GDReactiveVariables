0528a9b87fedc04db9fb3c336f7b264c23f17f2a
ryosanhin <90621212+ryosanhin@users.noreply.github.com>
2026-06-20 13:08:21 +0900

    feat: 購読管理用クラスを追加

--- addons/gd_reactive_variables/scripts/subscription.gd
+++ addons/gd_reactive_variables/scripts/subscription.gd
@@ -4,8 +4,11 @@ class_name Subscription
 var _disposes: Array[Callable]
 
 var _is_disposed := false
+## この購読が破棄されているか
 var is_disposed: bool:
 	get: return _is_disposed
+
+const META_KEY := "__subscription_manager__"
 
 ## [params disposes]: 購読の解除用メソッド（可変長変数）
 func _init(...disposes) -> void:
@@ -17,12 +20,29 @@ func dispose() -> void:
 		return
 	
 	_is_disposed = true
-	
 	for d in _disposes:
 		if d.is_valid():
 			d.call()
 	_disposes.clear()
+	print("disposed")
 
 ## 自動破棄登録
 func add_to(node: Node) -> void:
-	node.tree_exited.connect(dispose)
+	var subscription_manager := _find_subscription_manager(node)
+	subscription_manager.add_subscription(self)
+
+## [code]node[/code]の子ノードから[code]SubscriptionManager[/code]を探す[br]
+## [param node]: 紐づけ対象の[code]node[/code][br]
+## returns: そのノードの[code]SubscriptionManager[/code]
+func _find_subscription_manager(node: Node) -> SubscriptionManager:
+	var subscription_manager: SubscriptionManager = (
+		node.get_meta(META_KEY) if node.has_meta(META_KEY) else null
+	)
+	
+	if is_instance_valid(subscription_manager):
+		return subscription_manager
+	
+	var new_manager := SubscriptionManager.new()
+	node.add_child(new_manager)
+	node.set_meta(META_KEY, new_manager)
+	return new_manager
